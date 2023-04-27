3890672c74b4efc8f6fd55548f73065a490aed1f
Kalana <kalanaheshan47@gmail.com>
2023-04-27 22:12:57 +0530

    push without mongo db

--- database.js
+++ database.js
@@ -1,12 +1,5 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const mongoose = require("mongoose");
-const client = new MongoClient(process.env.DB, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+
 async function createConnection() {
   try {
     mongoose.connect(process.env.DB, {}, () => console.log("DB connected"));
